5a9eedd66c35a3de116049555b760f6cd29a6f4d
yuwancumian <easier2005@163.com>
2016-06-28 15:15:34 +0800

    basic feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 #!/usr/bin/env node
+import cfg from '../config'
+import fs from 'fs'
 
-const a = "Coming soon..."
-console.log(a);
+function copy(){
+    let snip
+    let item = process.argv[2]
+    if (!process.argv[2] || process.argv[2] === "") {
+        console.log('Sorry, a filename must be assigned!')
+        return
+    }
+    if (!!process.argv[3]){
+        let item = process.argv[2]
+        let snip = process.argv[3]
+        cfg[item] = snip
+        fs.writeFile('config.json',JSON.stringify(cfg,null,4),function(err){
+            if (err) throw err;
+            console.log(item +' was cached!');
+        })
+    }
+}
 
-export default a;
+export default copy();

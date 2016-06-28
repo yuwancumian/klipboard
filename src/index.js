b96d67a0d8df17cab33c2b2d5e567dcf9e0e4c49
yuwancumian <easier2005@163.com>
2016-06-28 21:52:31 +0800

    add copy feather

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import cfg from '../config'
 import fs from 'fs'
 import path from 'path'
+import clip from 'cliparoo'
+import chalk from 'chalk'
 
 function copy(){
     let snip
@@ -10,16 +12,30 @@ function copy(){
         console.log('Sorry, a filename must be assigned!')
         return
     }
+    if (process.argv[2] === 'ls'){
+        console.log('ls') 
+        return 
+    }
+    if (cfg[item] === undefined) {
+        console.log( "Sorry, " + item + " was not uncached ...")
+        return 
+    }
+    
     if (!!process.argv[3]){
         let item = process.argv[2]
         let snip = process.argv[3]
         let config = path.resolve(__dirname,'../config.json')
+        
         cfg[item] = snip
         fs.writeFile(config,JSON.stringify(cfg,null,4),function(err){
-            if (err) throw err;
-            console.log(item +' was cached!');
+            if (err) throw err
+            console.log(item +' was cached!')
         })
     }
+    clip(cfg[item],function(err){
+        if (err) throw err
+        console.log(chalk.green('copied'))
+    });
 }
 
-export default copy();
+export default copy()
